c14911c6dea2a27a042bf838828fda9b7fea1f86
sakshi gupta <info.corplyxtechnologies@gmail.com>
2023-07-11 13:02:37 +0530

    first commit

---
+++ about-us.php
@@ -0,0 +1,101 @@
+<?php include 'header.php' ?>
+
+<div class="inner-banner inner-bg1">
+<div class="container">
+<div class="inner-title text-center">
+<h3>About Us</h3>
+<ul>
+<li>
+<a href="index.php">Home</a>
+</li>
+<li>
+<i class='bx bxs-chevron-right'></i>
+</li>
+<li>About Us</li>
+
+</ul>
+</div>
+</div>
+</div>
+
+
+<div class="about-area pt-100 pb-70">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-6">
+<div class="about-img-2">
+<img src="assets/img/Best-Cloud-Storage-Services-Badge1.jpg" alt="">
+</div>
+</div>
+<div class="col-lg-6">
+<div class="about-content">
+<div class="section-title">
+
+
+<h2>About Us</h2>
+<b>GSTIN NO. : 27ABHCS9825N1ZH4</b>
+<p>Cornerview Solutions is a best Mumbai-based Cloud Space services provider and website development agency providing solutions for corporate website development, mobile app development.</p>
+<p>Cloud storage is a service model in which data is transmitted and stored on remote storage systems, where it is maintained, managed, backed up and made available to users over a network -- typically, the internet. Users generally pay for their cloud data storage on a per-consumption, monthly rate.</p>
+
+</div>
+
+</div>
+</div>
+</div>
+ </div>
+</div>
+
+
+
+<div class="appointment-area appointment-bg pt-100 pb-70">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-7">
+<div class="appointment-form">
+<div class="section-title">
+<h2>Quick Contact</h2>
+
+</div>
+<form action="#" method="post" >
+<div class="row">
+<div class="col-lg-6 col-sm-6">
+<div class="form-group">
+<input type="text" name="name" id="name" class="form-control" required="" data-error="Please enter your name" placeholder="Name">
+</div>
+</div>
+<div class="col-lg-6 col-sm-6">
+<div class="form-group">
+<input type="text" name="phone_number" id="phone_number" required="" data-error="Please enter your number" class="form-control" placeholder="Phone">
+</div>
+</div>
+<div class="col-lg-12 col-sm-12">
+<div class="form-group">
+<input type="email" name="email" id="email" class="form-control" required="" data-error="Please enter your email" placeholder="Email">
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<div class="form-group">
+<textarea name="message" class="form-control" id="message" cols="30" rows="8" required="" data-error="Write your message" placeholder="Your Message"></textarea>
+</div>
+</div>
+<div class="col-lg-12 col-md-12">
+<input type="submit" name="Send" class="default-btn" value="Send Message">
+
+</div>
+</div>
+</form>
+</div>
+</div>
+<div class="col-lg-5">
+<div class="appointment-img">
+<img src="assets/img/support.png" alt="">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<?php include 'footer.php' ?>
